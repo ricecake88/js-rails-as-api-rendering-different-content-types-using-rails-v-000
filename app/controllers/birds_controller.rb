0a9b3ff8c99a89e229924931e993b6bb0afd742f
Grace Shih <vocalise@gmail.com>
2020-01-15 07:47:47 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,11 @@
 class BirdsController < ApplicationController
   def index
+<<<<<<< HEAD
     birds = Bird.all
     render json: birds
+=======
+    @birds = Bird.all
+    render plain: "Hello #{@birds[3].name}"
+>>>>>>> c63fa0359f651b63f6a0aa8a0b7a38a6999f7ded
   end
 end
